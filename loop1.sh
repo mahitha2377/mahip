45d7f008250e563680ea78e9d2dee00d3a95960e
Your Name <you@example.com>
2025-04-10 14:46:22 +0530

    version 01

---
+++ loop1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#str1="kbn"
+#str2="kbn"
+echo " enter string 01 : "
+read  str1
+echo " enter string 02 : "
+read str2
+if [ " $ str1 " = " $ str2 " ]; then 
+	echo "both strings are equal "
+else
+	echo " both strings are not equal"
+fi
+
+
